04d3555b45a8d0669a022605e9a84af009a51fe1
abrarmusa <abrar.musa.89@gmail.com>
2020-07-07 12:34:44 +0600

    added schemas

---
+++ models/models/listings/ExpiredListing.js
@@ -0,0 +1,95 @@
+const mongoose = require('mongoose');
+const slug = require('mongoose-slug-updater');
+mongoose.plugin(slug);
+
+const ExpiredListingSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'An item must have a title!'],
+    trim: true,
+    maxlength: [30, 'A title must be less than 30 characters!']
+  },
+  slug: {
+    unique: true,
+    type: String,
+    slug: 'title'
+  },
+  winner: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'user'
+  },
+  description: {
+    type: String,
+    required: [true, 'An item must have a description!'],
+    trim: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+
+  image: {
+    type: String
+  },
+  currentPrice: { type: Number },
+  startPrice: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  minIncrement: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  createdBy: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'user'
+  },
+  category: {
+    type: String
+  },
+  endDate: {
+    type: Date,
+    required: true
+  },
+  condition: {
+    type: String,
+    enum: ['unspecified', 'used', 'new'],
+    default: 'unspecified'
+  },
+  bids: [
+    {
+      user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'user',
+        required: true
+      },
+      bid: {
+        type: Number,
+        required: true
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now()
+      }
+    }
+  ],
+  verified_by: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'admin'
+  },
+  active: { type: Boolean, default: false }
+});
+
+//sets the current price to the starting price
+ExpiredListingSchema.pre('save', function(next) {
+  if (!this.currentPrice) {
+    this.currentPrice = this.startPrice;
+  }
+  next();
+});
+
+const ExpiredListing = mongoose.model('ExpiredListing', ExpiredListingSchema);
+
+module.exports = ExpiredListing;
---
+++ models/models/users/User.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0'],
+    set: val => Math.round(val * 10) / 10
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  location: {
+    type: String
+  },
+  bio: {
+    type: String
+  },
+  role: {
+    type: String,
+    enum: ['Bidder', 'Buyer', 'Auctioneer', 'Administrator'],
+  },
+  privileges: {
+    type: String,
+    required: function (){ return this.role === 'Administrator'; }
+  }
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
